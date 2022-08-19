1239a9d72a102c943a8e44fdd30ac60be7db0b05
Jesús Mares <maresdf@comunidad.unam.mx>
2022-08-19 14:25:29 -0500

    particles

--- main.js
+++ main.js
@@ -2,10 +2,25 @@
 
 // toggle button
 const toggle = document.getElementById("nav-toggle"),
-  collapse = document.getElementById("nav-collapse");
+  collapse = document.getElementById("nav-collapse"),
+  body = document.querySelector("body"),
+  particles = document.createElement("tag");
 
 if (toggle) {
   toggle.addEventListener("click", () => {
     collapse.classList.toggle("active");
   });
 }
+
+document.addEventListener("mousemove", function (e) {
+  let x = e.offsetX;
+  let y = e.offsetY;
+  particles.style.left = x + "px";
+  particles.style.top = y + "px";
+
+  body.appendChild(particles);
+
+  setTimeout(() => {
+    particles.remove();
+  }, 2000);
+});
